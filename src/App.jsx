5e835bb42639b4e6c08fdd0dd477776c0c70d5e7
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-04-16 13:13:33 +0300

    Create App.jsx structure and disable prop types warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MyThemeProvider from "./providers/theme/MyThemeProvider";
 
-function App() {
-  return <></>;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <MyThemeProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <></>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </MyThemeProvider>
+  );
 }
-
-export default App;

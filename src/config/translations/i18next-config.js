ea3caf33585e3715a114d84ef6ee39301678ad47
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-04-16 21:24:50 +0300

    Install i18next language detector

--- src/config/translations/i18next-config.js
+++ src/config/translations/i18next-config.js
@@ -1,7 +1,8 @@
-import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./config/locales/en.json";
+
+import en from "./locales/en.json";
 
 const resources = {
   en: {

5f2e2a28ecbb22fb1738bbb076a7bfe917647dd1
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-04-16 18:35:02 +0300

    Update the path for i18next config and add jsonconfig for imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MyThemeProvider from "./providers/theme/MyThemeProvider";
+import "@/config/translations/i18next-config.js";
 
 const queryClient = new QueryClient({
   defaultOptions: {

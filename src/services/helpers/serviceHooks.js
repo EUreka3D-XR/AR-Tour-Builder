11e56686edc220d03ecb12882e970653d7ec59e2
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-09-23 21:00:16 +0300

    improve serviceHooks with a stale parameter

--- src/services/helpers/serviceHooks.js
+++ src/services/helpers/serviceHooks.js
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 
 import { useFetchStateHelper } from "@/hooks/useFetchState";
 
+const STORE_VALUE_TIME = 20 * 60 * 1000; // 20 minutes
+
 /**
  * @typedef {import("@/types/jsdoc-types").FetchStateType} FetchStateType
  * @template T
@@ -14,12 +16,14 @@ import { useFetchStateHelper } from "@/hooks/useFetchState";
  * @param {Object} params
  * @param {() => Promise<T>} params.fetcher - The function to fetch data that returns Promise<T>
  * @param {String[]} params.queryKey - The key for the query (queryKey for useQuery).
+ * @param {boolean} [params.shouldStoreValue=false] - Whether to store the fetched value for a certain time and not refetch it.
  * @returns {FetchResultType<T>} - Object containing fetched data and fetch state.
  */
-export const useDataFetcher = ({ fetcher, queryKey }) => {
+export const useDataFetcher = ({ fetcher, queryKey, shouldStoreValue }) => {
   const { data, ...queryResponse } = useQuery({
     queryKey,
     queryFn: fetcher,
+    staleTime: shouldStoreValue ? STORE_VALUE_TIME : 0,
   });
   const { fetchState } = useFetchStateHelper(queryResponse);
 

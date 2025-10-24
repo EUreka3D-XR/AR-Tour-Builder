fa58d91242d82b73740e333911e0fedee05c8f41
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-10-24 13:30:31 +0300

    move timestamps in tourheader below right action buttons

--- src/pages/tour/_sections/TourHeaderSection.jsx
+++ src/pages/tour/_sections/TourHeaderSection.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useFormContext, useWatch } from "react-hook-form";
-import { IconButton, styled, Typography } from "@mui/material";
+import { IconButton, Stack, styled, Typography } from "@mui/material";
 
 import Button from "@/components/button/Button";
 import EurekaIcon from "@/components/icon/EurekaIcon";
@@ -10,7 +10,7 @@ import { dateFormatters } from "@/utils/datetimeFormatters";
 const ContainerStyled = styled("div")(({ theme }) => ({
   padding: theme.spacing(1.5, 2),
   display: "flex",
-  alignItems: "center",
+  alignItems: "flex-start",
   justifyContent: "space-between",
   gap: theme.spacing(4),
   overflow: "hidden",
@@ -22,8 +22,12 @@ const ContainerStyled = styled("div")(({ theme }) => ({
   },
   "& .right-section": {
     display: "flex",
-    alignItems: "center",
-    gap: theme.spacing(1),
+    flexDirection: "column",
+    alignItems: "flex-end",
+    gap: theme.spacing(0.5),
+    "& .timestamps": {
+      fontStyle: "italic",
+    },
   },
 }));
 
@@ -58,38 +62,42 @@ function TourHeaderSection({ onSave, onPublish, onArchive }) {
       </div>
       {isExisting && (
         <div className="right-section">
-          {"Last modified: "}
-          {dateFormatters.lastUpdatedLike(lastModifiedAt)}
-          <Button
-            startIcon={<EurekaIcon name="save" />}
-            type="submit"
-            isLoading={isSubmitting}
-          >
-            Save Changes
-          </Button>
-          <Button
-            startIcon={<EurekaIcon name="delete" />}
-            color="error"
-            onClick={onSave}
-          >
-            Delete Tour
-          </Button>
-          {tourStatus === "draft" && (
+          <Stack direction="row" alignItems="center" spacing={1}>
             <Button
-              startIcon={<EurekaIcon name="publish" />}
-              onClick={onPublish}
+              startIcon={<EurekaIcon name="save" />}
+              type="submit"
+              isLoading={isSubmitting}
             >
-              Publish
+              Save Changes
             </Button>
-          )}
-          {tourStatus === "published" && (
             <Button
-              startIcon={<EurekaIcon name="archive" />}
-              onClick={onArchive}
+              startIcon={<EurekaIcon name="delete" />}
+              color="error"
+              onClick={onSave}
             >
-              Archive
+              Delete Tour
             </Button>
-          )}
+            {tourStatus === "draft" && (
+              <Button
+                startIcon={<EurekaIcon name="publish" />}
+                onClick={onPublish}
+              >
+                Publish
+              </Button>
+            )}
+            {tourStatus === "published" && (
+              <Button
+                startIcon={<EurekaIcon name="archive" />}
+                onClick={onArchive}
+              >
+                Archive
+              </Button>
+            )}
+          </Stack>
+          <Typography className="timestamps">
+            {"Last modified: "}
+            {dateFormatters.lastUpdatedLike(lastModifiedAt)}
+          </Typography>
         </div>
       )}
     </ContainerStyled>

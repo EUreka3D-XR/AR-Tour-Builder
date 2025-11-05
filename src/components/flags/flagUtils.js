af04fb5b5931c730cfb5848d8e0263c6753aa48c
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-11-05 20:07:05 +0200

    delete flagUtils

--- src/components/flags/flagUtils.js
+++
@@ -1,7 +0,0 @@
-export const getFlagUrl = (locale) => {
-  const loc = LocalesDictionary[locale] || locale;
-  return `https://flagcdn.com/${loc}.svg`;
-};
-export const LocalesDictionary = {
-  en: "gb",
-};

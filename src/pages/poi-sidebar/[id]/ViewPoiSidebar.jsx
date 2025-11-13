eb99cc594edef2faaae28069d6689eb6a710575a
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-11-13 11:27:44 +0200

    open edit poi asset from view poi sidebar

--- src/pages/poi-sidebar/[id]/ViewPoiSidebar.jsx
+++ src/pages/poi-sidebar/[id]/ViewPoiSidebar.jsx
@@ -1,16 +1,28 @@
 import SidebarHeader from "@/components/sidebar/_sections/SidebarHeaderSection";
 import SidebarFormArea from "../_common/_utils/SidebarFormArea";
+import ToggleVisibility from "../_common/_utils/ToggleVisibility";
 import PoiSidebar from "../_common/sidebar";
+import EditPoiAssetForm from "../_forms/poi-asset/EditPoiAssetForm";
 import ViewPoiSidebarContainer from "./container";
 
 function ViewPoiSidebar() {
   return (
     <PoiSidebar>
-      {({ onClosePoi }) => {
+      {({ onClosePoi, onCloseAsset, showPoiForm, showEditAssetForm }) => {
         return (
           <>
             <SidebarHeader title="Point of Interest" onClose={onClosePoi} />
             <SidebarFormArea className="sidebar-form-area">
+              <ToggleVisibility show={showPoiForm}>
+                <div className="main-area">
+                  <ViewPoiSidebarContainer />
+                </div>
+              </ToggleVisibility>
+              <div className="main-area">
+                {showEditAssetForm && (
+                  <EditPoiAssetForm onClose={onCloseAsset} />
+                )}
+              </div>
               <div className="main-area">
                 <ViewPoiSidebarContainer />
               </div>
--- src/pages/poi-sidebar/[id]/_sections/ViewPoiMedia.jsx
+++ src/pages/poi-sidebar/[id]/_sections/ViewPoiMedia.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router";
 import { Stack } from "@mui/material";
 
 import NotFoundText from "../_common/NotFoundText";
@@ -5,13 +6,27 @@ import SectionTitle from "../_common/SectionTitle";
 import MediaCardItem from "../../_common/_tabs/_media/MediaCardItem";
 
 function ViewPoiMedia({ mediaItems = [] }) {
+  const [, setSearchParams] = useSearchParams();
+
+  const handleEditPoiAsset = (asset) => {
+    setSearchParams((prev) => {
+      prev.set("mediaForm", "edit");
+      prev.set("mediaId", asset.id);
+      return prev;
+    });
+  };
+
   return (
     <div className="poi-media-section">
       <SectionTitle>{`Asset Media (${mediaItems.length})`} </SectionTitle>
       {mediaItems.length > 0 ? (
         <Stack spacing={1}>
           {mediaItems.map((asset) => (
-            <MediaCardItem key={asset.id} asset={asset} />
+            <MediaCardItem
+              key={asset.id}
+              asset={asset}
+              onEdit={handleEditPoiAsset}
+            />
           ))}
         </Stack>
       ) : (

92fdecc234f5ed8a84df10c34a1857082c03c25d
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-12-02 19:54:44 +0200

    fix api calls payload

--- src/api/calls/authApi.js
+++ src/api/calls/authApi.js
@@ -1,14 +1,14 @@
 import { baseUrls } from "../endpoints-base-urls/baseUrls";
 import fetcher from "../fetcher/api-fetcher";
 
-const login = async (payload) => {
+const login = async (data) => {
   const url = baseUrls.login;
-  return fetcher.post(url, payload);
+  return fetcher.post(url, { data });
 };
 
-const signup = async (payload) => {
+const signup = async (data) => {
   const url = baseUrls.signup;
-  return fetcher.post(url, payload);
+  return fetcher.post(url, { data });
 };
 
 const logout = async () => {

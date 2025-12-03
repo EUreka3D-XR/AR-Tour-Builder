b5a8cd9ab6ba3e243b64a83ad403e87fe019e116
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-12-03 12:07:11 +0200

    add missing mutation calls for projects

--- src/api/calls/projectsApi.js
+++ src/api/calls/projectsApi.js
@@ -11,7 +11,25 @@ const getProject = async (projectId, locale) => {
   return fetcher.get(url, { locale });
 };
 
+const createProject = async (data, locale) => {
+  const url = baseUrls.projects;
+  return fetcher.post(url, { data, locale });
+};
+
+const updateProject = async (projectId, data, locale) => {
+  const url = baseUrls.project(projectId);
+  return fetcher.put(url, { data, locale });
+};
+
+const deleteProject = async (projectId) => {
+  const url = baseUrls.project(projectId);
+  return fetcher.delete(url);
+};
+
 export const projectsApi = {
   fetchAll: getAllProjects,
   fetchOne: getProject,
+  create: createProject,
+  update: updateProject,
+  delete: deleteProject,
 };
--- src/services/projectsService.js
+++ src/services/projectsService.js
@@ -1,7 +1,7 @@
 import { api } from "@/api";
 
 import { useLocale } from "@/hooks/useLocale";
-import { useDataFetcher } from "./helpers/serviceHooks";
+import { useDataFetcher, useDataMutator } from "./helpers/serviceHooks";
 
 /**
  * @typedef {import('@/types/jsdoc-types').Project} Project
@@ -52,6 +52,41 @@ const useProjectMultilingual = (projectId) => {
   return useDataFetcher({
     fetcher: () => api.projects.fetchOne(projectId),
     queryKey: ["project", projectId, "multilingual"],
+  });
+};
+
+/**
+ * @returns {ProjectResult}
+ */
+export const useCreateProject = () => {
+  return useDataMutator({
+    fetcher: ({ data }) => api.projects.create(data),
+    mutationKey: ["create-project"],
+    invalidateKey: ["projects"],
+  });
+};
+
+/**
+ * @param {string} projectId
+ * @returns {ProjectResult}
+ */
+export const useUpdateProject = (projectId) => {
+  return useDataMutator({
+    fetcher: ({ data }) => api.projects.update(projectId, data),
+    mutationKey: ["update-project", projectId],
+    invalidateKey: ["project", projectId],
+  });
+};
+
+/**
+ * @param {string} projectId
+ * @returns {ProjectResult}
+ */
+export const useDeleteProject = (projectId) => {
+  return useDataMutator({
+    mutator: () => api.projects.delete(projectId),
+    mutationKey: ["delete-project", projectId],
+    invalidateKey: ["projects"],
   });
 };
 

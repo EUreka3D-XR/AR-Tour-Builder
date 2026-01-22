523f5536730b1f7fcb13f389d50d6f606a9b7022
Leonidas Mavrotas <leo.mav.8@gmail.com>
2026-01-22 12:57:04 +0200

    actually update library table type column to have more width, previous commit was about sending null georeference if lat long are empty

--- src/pages/library/_sections/_table-section/LibraryTableSection.jsx
+++ src/pages/library/_sections/_table-section/LibraryTableSection.jsx
@@ -1,6 +1,6 @@
-import { Box } from "@mui/material";
-import { useTranslation } from "react-i18next";
 import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { Box } from "@mui/material";
 
 import Table from "@/components/table/Table";
 import useDashboardParams from "@/hooks/useDashboardParams";
@@ -12,38 +12,46 @@ function LibraryTableSection({ assets = [], total, fetchState }) {
   const { t } = useTranslation();
   const { filterParams, updateParams } = useDashboardParams();
 
-  const columns = useMemo(() => [
-    { label: t("library.table.columns.title"), value: "title", width: 250, wrap: true },
-    {
-      label: t("library.table.columns.type"),
-      value: "type",
-      width: 120,
-      disableSorting: true,
-      renderCell: (row) => <AssetTypeCell type={row.type} />,
-    },
-    {
-      label: t("library.table.columns.url"),
-      value: "contentUrl",
-      width: 300,
-      disableSorting: true,
-      renderCell: (row) => <AssetURLCell url={row.contentUrl} />,
-    },
-    {
-      label: t("library.table.columns.filename"),
-      value: "filename",
-      minWidth: 300,
-      disableSorting: true,
-    },
-    {
-      label: "",
-      value: "action",
-      disableSorting: true,
-      width: 150,
-      renderCell: (asset) => {
-        return <AssetActionsCell assetId={asset.id} />;
+  const columns = useMemo(
+    () => [
+      {
+        label: t("library.table.columns.title"),
+        value: "title",
+        width: 250,
+        wrap: true,
       },
-    },
-  ], [t]);
+      {
+        label: t("library.table.columns.type"),
+        value: "type",
+        width: 160,
+        disableSorting: true,
+        renderCell: (row) => <AssetTypeCell type={row.type} />,
+      },
+      {
+        label: t("library.table.columns.url"),
+        value: "contentUrl",
+        width: 300,
+        disableSorting: true,
+        renderCell: (row) => <AssetURLCell url={row.contentUrl} />,
+      },
+      {
+        label: t("library.table.columns.filename"),
+        value: "filename",
+        minWidth: 300,
+        disableSorting: true,
+      },
+      {
+        label: "",
+        value: "action",
+        disableSorting: true,
+        width: 150,
+        renderCell: (asset) => {
+          return <AssetActionsCell assetId={asset.id} />;
+        },
+      },
+    ],
+    [t],
+  );
 
   return (
     <Box sx={{ flex: 1 }}>

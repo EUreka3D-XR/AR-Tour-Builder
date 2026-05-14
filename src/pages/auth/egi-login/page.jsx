6c19f7246a2621d63ebb6c6fc8e7fe9bdc6ed2dc
Leonidas Mavrotas <leo.mav.8@gmail.com>
2026-05-14 15:31:33 +0300

    add the logo in the egi-login page

--- src/pages/auth/egi-login/page.jsx
+++ src/pages/auth/egi-login/page.jsx
@@ -1,11 +1,24 @@
 import { useEffect, useRef, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
-import { Alert, Box, CircularProgress, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  CircularProgress,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
 
 import { useEGILogin } from "@/services/authService";
 import Button from "@/components/button/Button";
 import useNavPaths from "@/hooks/useNavPaths";
 import { extractEGICallback } from "@/utils/egiAuth";
+
+const Logo = styled(Box)({
+  height: 180,
+  width: "auto",
+  objectFit: "contain",
+});
 
 function EGICallbackPage() {
   const { t } = useTranslation();
@@ -20,7 +33,6 @@ function EGICallbackPage() {
 
     try {
       const { code, state, codeVerifier } = extractEGICallback();
-      setAccessDenied(true);
       egiLogin(
         { code, state, codeVerifier },
         {
@@ -49,44 +61,49 @@ function EGICallbackPage() {
       width="100vw"
       gap={3}
     >
-      {accessDenied ? (
-        <Box
-          maxWidth={480}
-          width="100%"
-          display="flex"
-          flexDirection="column"
-          gap={2}
-        >
-          <Alert severity="error">
-            {t(
-              "auth.login.egiAccess.noAccess",
-              "An error has occurred, you don't have access to the AR Tour Builder Space",
-            )}
-          </Alert>
-          <Alert severity="info">
-            <Typography variant="body2" sx={{ whiteSpace: "pre-line" }}>
-              <Trans i18nKey="auth.login.egiAccess.requestAccess" />
-            </Typography>
-          </Alert>
-          <Button
-            href="https://go.egi.eu/join_eureka3d"
-            target="_blank"
-            rel="noopener"
+      <Stack alignItems="center" spacing={1}>
+        <Logo component="img" src={"/logo.webp"} alt={t("auth.logoAlt")} />
+        {accessDenied ? (
+          <Box
+            maxWidth={480}
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            gap={2}
           >
-            {t("auth.login.egiAccess.requestAccessLink", "Request access")}
-          </Button>
-        </Box>
-      ) : (
-        <>
-          <CircularProgress />
-          <Typography variant="h6">
-            {t(
-              "auth.login.egiAccess.completing",
-              "Completing EGI login, please wait...",
-            )}
-          </Typography>
-        </>
-      )}
+            <Alert severity="error">
+              {t(
+                "auth.login.egiAccess.noAccess",
+                "An error has occurred, you don't have access to the AR Tour Builder Space",
+              )}
+            </Alert>
+            <Alert severity="info">
+              <Typography variant="body2" sx={{ whiteSpace: "pre-line" }}>
+                <Trans i18nKey="auth.login.egiAccess.requestAccess" />
+              </Typography>
+            </Alert>
+            <Button
+              variant="filled"
+              corners="round"
+              href="https://go.egi.eu/join_eureka3d"
+              target="_blank"
+              rel="noopener"
+            >
+              {t("auth.login.egiAccess.requestAccessLink", "Request access")}
+            </Button>
+          </Box>
+        ) : (
+          <>
+            <CircularProgress />
+            <Typography variant="h6">
+              {t(
+                "auth.login.egiAccess.completing",
+                "Completing EGI login, please wait...",
+              )}
+            </Typography>
+          </>
+        )}
+      </Stack>
     </Box>
   );
 }
